9cfb097788d7198805ddae362034eebb3b316ed8
Nalivela,Saikumar <s533628@ol10033m.nwmissouri.edu>
2019-02-14 22:17:21 -0600

    Initial Commit

---
+++ The Arithmetic AppTests/The_Arithmetic_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Arithmetic_AppTests.swift
+//  The Arithmetic AppTests
+//
+//  Created by Nalivela,Saikumar on 2/14/19.
+//  Copyright © 2019 Nalivela,Saikumar. All rights reserved.
+//
+
+import XCTest
+@testable import The_Arithmetic_App
+
+class The_Arithmetic_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
